276304332a74055e5f60181a0dc0a24afbf900d3
jbielak <justyna_bielak@outlook.com>
2018-09-23 23:40:33 +0200

    Add base entity. Closes #10

--- pet-clinic-data/src/main/java/com/jbielak/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/jbielak/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.jbielak.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private LocalDate birthDate;
     private PetType petType;
