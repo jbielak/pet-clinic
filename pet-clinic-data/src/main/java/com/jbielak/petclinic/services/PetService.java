28989ce590402413a1969da06ee78e4541add90e
jbielak <justyna_bielak@outlook.com>
2018-09-24 18:29:25 +0200

    Refactor to common crud service interface. Closes #11

--- pet-clinic-data/src/main/java/com/jbielak/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/jbielak/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.jbielak.petclinic.services;
 
 import com.jbielak.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findbyId(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
