b79d6b2ec99fa3396ade2f3be2832217d4cd2c20
jbielak <justyna_bielak@outlook.com>
2018-09-24 18:51:29 +0200

    Implement Map based Services. Closes #6

---
+++ pet-clinic-data/src/main/java/com/jbielak/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package com.jbielak.petclinic.services.map;
+
+import com.jbielak.petclinic.model.Pet;
+import com.jbielak.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long>
+        implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
