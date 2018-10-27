dada250c1a8565a23c99793c297e00281be8eaba
jbielak <justyna_bielak@outlook.com>
2018-10-27 20:07:56 +0200

    Implement find Owner. Closes #56

--- pet-clinic-data/src/main/java/com/jbielak/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/jbielak/petclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package com.jbielak.petclinic.services;
 
 import com.jbielak.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService  extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
